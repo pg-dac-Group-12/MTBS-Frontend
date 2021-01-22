5b897d6fb42a9a018ccb4ca91f01eb104c8ac551
Rohan Julka <rohanjulka19@gmail.com>
2021-01-23 01:25:26 +0530

    Added MovieState

---
+++ src/app/states/MovieState.ts
@@ -0,0 +1,31 @@
+import { Movie } from "../models/movie.model"
+import {BehaviorSubject, Subject} from 'rxjs' 
+
+export class MovieState {
+    private movies$ = new BehaviorSubject<Movie[]>([]);
+
+    getMovies() {
+        return this.movies$.asObservable();
+    }
+
+    setMovies(movies :Movie[]) {
+        this.movies$.next(movies);
+    }
+
+    addMovie(movie: Movie) {
+        const currentValue = this.movies$.getValue();
+        this.movies$.next([...currentValue, movie]);
+    }
+
+    updateMovie(movie:Movie) {
+        const currentMoviesList = this.movies$.getValue();
+        const indexOfUpdated = currentMoviesList.findIndex(movieItem => movie.id == movieItem.id  ); 
+        currentMoviesList[indexOfUpdated] = movie ;
+    }
+
+    updateMovieById(id:number, updatedMovie:Movie) {
+        const currentMoviesList = this.movies$.getValue();
+        const indexToUpdate = currentMoviesList.findIndex(movie => movie.id == id);
+        currentMoviesList[indexToUpdate] = updatedMovie ;
+    }
+}
