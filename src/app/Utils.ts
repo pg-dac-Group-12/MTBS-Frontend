b6fdec8a3ccd5722ff6638a2c23ea0338f1db364
serarthurdayne <kewalramanitanay@gmail.com>
2021-01-23 06:48:22 +0530

    Implemented Response status code validation on ticket service

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,19 +1,23 @@
 import { HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-class ResponseUtils {
-      obj:Record<number,any> = {
-        200 : function (x:HttpResponse<any>) {
-            return x.body;
+export class Utils {
+    static obj:Record<number,any> = {
+        200 : function (response:HttpResponse<any>) {
+            return response.body;
         },
-        201: (x:HttpResponse<any>)=> {
-            return x.body;
+        201: (response:HttpResponse<any>)=> {
+            return response.body;
         },
-        401: (x:HttpResponse<any>)=>{
-            
+        401: (response:HttpResponse<any>)=>{
+            //directives
+            return null;
         }
     }
 
-     responseCheck(response: HttpResponse<any>): any {
-      return this.obj[response.status]();
+   static validateResponse(observable: Observable<HttpResponse<any>>): any {
+         return observable.subscribe(
+             (response)=>{return this.obj[response.status]()}
+         )
     }
 }
--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Ticket } from '../models/ticket.model';
 import { Shows } from '../models/shows.model';
+import { Utils} from '../Utils';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,21 +11,21 @@ export class TicketService {
   baseUrl: string = "http://localhost:8080/ticket/";
   constructor(private http: HttpClient) { }
 
-  getAllTicketsByUserId(userId: number): Observable<HttpResponse<Ticket>> {
+  getAllTicketsByUserId(userId: number): Ticket {
     let reqParams = new HttpParams();
     reqParams.append('userId', String(userId));
-    return this.http.get<Ticket>(`${this.baseUrl}`, { observe: 'response', params: reqParams });
+    return Utils.validateResponse(this.http.get<Ticket>(`${this.baseUrl}`, { observe: 'response', params: reqParams }));
   }
-  getTicketById(id: number): Observable<HttpResponse<Ticket>> {
-    return this.http.get<Ticket>(`${this.baseUrl}${id}`, { observe: 'response' });
+  getTicketById(id: number):Ticket {
+    return Utils.validateResponse(this.http.get<Ticket>(`${this.baseUrl}${id}`, { observe: 'response' }));
   }
-  createTicket(showId: number, seats: number[]): Observable<HttpResponse<Ticket>> {
+  createTicket(showId: number, seats: number[]): Ticket {
     let reqParams = new HttpParams();
     reqParams.append('seats', String(seats));
-    return this.http.post<Ticket>(`${this.baseUrl}${showId}`, null, { observe: 'response', params: reqParams });
+    return Utils.validateResponse(this.http.post<Ticket>(`${this.baseUrl}${showId}`, null, { observe: 'response', params: reqParams }));
   }
-  cancelTicket(ticket: Ticket): Observable<HttpResponse<Ticket>> {
-    return this.http.post<Ticket>(`${this.baseUrl}`, ticket, { observe: 'response' });
+  cancelTicket(ticket: Ticket): Ticket{
+    return Utils.validateResponse(this.http.post<Ticket>(`${this.baseUrl}`, ticket, { observe: 'response' }));
   }
 }
 
