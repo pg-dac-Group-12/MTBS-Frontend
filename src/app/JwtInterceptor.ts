6543e4830fe8844ac281336908c35df8e72b969b
Rohan Julka <rohanjulka19@gmail.com>
2021-01-23 14:26:21 +0530

    Jwt integration

---
+++ src/app/JwtInterceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class JwtInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token = localStorage.getItem("id_token");
+        if (token != null) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+    
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { UserDashboardComponent } from '../user-workflow/user-dashboard/user-dashboard.component';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,12 @@ export class AuthService {
   
   constructor(private http:HttpClient) { }
   
-  authenticateUser(email:string,password:string,isThetreAdmin:boolean):Observable<User>{
+  authenticateUser(email:string,password:string,isThetreAdmin:boolean){
     let params = new HttpParams();
     params.append('email',email);
     params.append('password',password);
     params.append('isTheatrAdmin',String(isThetreAdmin));
-    return this.http.post<any>(`${this.baseUrl}`,null,{params:params});
+    //return this.setJWT(this.http.post<any>(`${this.baseUrl}`,null,{observe:'response',params:params}));
   }
 
   logOffUser():Observable<User>{
@@ -29,5 +30,16 @@ export class AuthService {
     params.append('newPassword',newPassword);
     return this.http.post<any>(`${this.baseUrl}password/change`,null,{observe:'response',params:params});
   }
-  
+ 
+  private setJWT(auth:Observable<HttpResponse<any>>) : HttpResponse<any> {
+    let user:User;
+    let response:HttpResponse<any>
+    auth.subscribe(resp =>{
+      response = resp ;
+      localStorage.setItem('id_token',resp.body);
+    })
+    // localStorage.setItem('id_token', auth.subscribe(auth=));
+    // localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+    return response!;
+  }          
 }
